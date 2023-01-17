53082102dfde2268a72905c204d89e8f0d2057f1
Bui Quang Hieu <hieubq6@ghtk.internal>
2023-01-17 14:52:46 +0700

    fix env

--- db.js
+++ db.js
@@ -2,12 +2,12 @@ const Pool = require("pg").Pool;
 const pool = new Pool({
   user: "postgres",
   host:
-    process.env.NODE_ENV === development
+    process.env.NODE_ENV === "development"
       ? "localhost"
       : process.env.SUPABASE_HOST,
-  database: process.env.NODE_ENV === development ? "perntodo" : "postgres",
+  database: process.env.NODE_ENV === "development" ? "perntodo" : "postgres",
   password:
-    process.env.NODE_ENV === development
+    process.env.NODE_ENV === "development"
       ? "Admin"
       : process.env.SUPABASE_DATABASE_PASSWORD,
   port: 5432,
